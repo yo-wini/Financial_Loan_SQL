6d583fee820dea5fb957020e77baf004d00a3eb9
yo-wini <yashaswiniii15@gmail.com>
2025-06-20 15:27:33 +0530

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,59 @@
+--LOAN STATUS
+SELECT
+        loan_status,
+        COUNT(id) AS LoanCount,
+        SUM(total_payment) AS Total_Amount_Received,
+        SUM(loan_amount) AS Total_Funded_Amount,
+        AVG(int_rate * 100) AS Interest_Rate,
+        AVG(dti * 100) AS DTI
+    FROM
+        loan
+    GROUP BY
+        loan_status;
+
+SELECT 
+	loan_status, 
+	SUM(total_payment) AS MTD_Total_Amount_Received, 
+	SUM(loan_amount) AS MTD_Total_Funded_Amount 
+FROM loan
+WHERE MONTH(issue_date) = 12 
+GROUP BY loan_status;
+
+--BANK LOAN REPORT(MONTH)
+
+SELECT 
+	MONTH(issue_date) AS Month_Munber, 
+	DATENAME(MONTH, issue_date) AS Month_name, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM loan
+GROUP BY MONTH(issue_date), DATENAME(MONTH, issue_date)
+ORDER BY MONTH(issue_date);
+
+--BANK LOAN REPORT(STATE)
+SELECT 
+	address_state AS State, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM loan
+GROUP BY address_state
+ORDER BY address_state;
+
+
+--EMPLOYEE LENGTH
+SELECT 
+	emp_length AS Employee_Length, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM loan
+GROUP BY emp_length
+ORDER BY emp_length;
+
+
+
+
+
 
